f1ffa39844db1ef364dfc2578a1e3547c76b822a
Manfred <d029940@users.noreply.github.com>
2024-03-25 20:00:39 +0100

    Pre release

--- BicycleCheckList/ViewModels/TourListViewModel.cs
+++ BicycleCheckList/ViewModels/TourListViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using BicycleCheckList.Models;
+using BicycleCheckList.Resources.Strings;
+using BicycleCheckList.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 
@@ -8,22 +11,51 @@ namespace BicycleCheckList.ViewModels
     public partial class TourListViewModel : BaseViewModel
     {
        
-        public TourList Tours { get; }
+        public TourList Tours { get; set;  }
 
         [ObservableProperty]
         Tour? selectedTour;
+
+        OverviewViewModel overviewViewModel;
 
         partial void OnSelectedTourChanged(Tour? oldValue, Tour? newValue)
         {
             Debug.WriteLine($"Changed: Old = {oldValue?.Name}, New = {newValue?.Name}");
         }
 
-        public TourListViewModel()
+        public TourListViewModel(OverviewViewModel overviewViewModel)
         {
+            this.overviewViewModel = overviewViewModel;
             this.Tours = new TourList();
             Tours.Load();
-            SelectedTour = Tours.AllTours?[Tours.CurrentTour];
-            Title = "Tour list";
+            // TODO: Currently hard-coded
+            SelectedTour = Tours.AllTours?[0];
+            Title = AppResources.TourList;
         }
+
+
+        [RelayCommand]
+        void Save()
+        {
+            // TODO: Currently hard-coded
+            //Tours!.AllTours![0].ItemGroupList =  SelectedTour!.ItemGroupList.ToList();
+            TourListService.WriteToJson(Tours);
+        }
+
+        [RelayCommand]
+        void Reset()
+        {
+            TourList res = TourListService.Reset();
+            if (res != null)
+            {
+                // TODO: Currently hard-coded
+                SelectedTour = res.AllTours?[0];
+                
+                overviewViewModel.CheckItemsGroups = new ObservableCollection<CheckItemGroup>(SelectedTour!.ItemGroupList);
+
+
+            }
+        }
+
     }
 }
--- BicycleCheckList/Views/TourListPage.xaml.cs
+++ BicycleCheckList/Views/TourListPage.xaml.cs
@@ -15,7 +15,7 @@ public partial class TourListPage : ContentPage
     //protected override void OnNavigatedTo(NavigatedToEventArgs args)
     //{
     //    var vm = BindingContext as TourListViewModel;
-    //    vm.SelectedTour = vm.Tours.AllTours[vm.Tours.CurrentTour];
+    //    vm.SelectedTour = vm.Tours.AllTours?[vm.Tours.CurrentTour];
     //    Debug.WriteLine($"SelectedTour = {vm.SelectedTour}");
     //    base.OnNavigatedTo(args);
     //}
