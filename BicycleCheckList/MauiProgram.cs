e5ec286a6e29cc274dfd9d37729ca971ccea0210
Manfred <d029940@users.noreply.github.com>
2024-04-02 12:58:24 +0200

    Google Font

--- BicycleCheckList/MauiProgram.cs
+++ BicycleCheckList/MauiProgram.cs
@@ -17,6 +17,7 @@ namespace BicycleCheckList
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MaterialIcons-Regular.ttf", "GoogleMaterialFont");
                 });
 
 #if DEBUG
